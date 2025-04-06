6d99b43fe88bb668dad003b9d88759f8c7c7b5d7
Aleksei Maksimov <aleksei.maksimov@axenix.pro>
2025-04-06 21:56:58 +0700

    Add fuzzy search

--- search_engine/search_engine.py
+++ search_engine/search_engine.py
@@ -4,8 +4,14 @@ import re
 def get_term(token: str) -> str:
     processed_token = re.findall(r'\w+', token)
     term = ''.join(processed_token).lower()
+    return term
 
-    return term.lower()
+
+def get_terms(tokens: list[str]) -> list[str]:
+    terms = []
+    for token in tokens:
+        terms.append(get_term(token))
+    return terms
 
 
 def process_docs(docs_dict: dict[str, str]) -> dict[str, list[str]]:
@@ -14,7 +20,6 @@ def process_docs(docs_dict: dict[str, str]) -> dict[str, list[str]]:
         terms = []
         for token in value.split():
             terms.append(get_term(token))
-
         processed_docs_dict[key] = terms
 
     return processed_docs_dict
@@ -27,21 +32,21 @@ def search(docs: list[dict[str, str]], string: str) -> list[str]:
     def count_term(_key: str) -> int:
         counter = 0
         for word in processed_docs_dict[_key]:
-            if not term == word:
+            if word not in terms:
                 continue
             counter += 1
-
         return counter
 
     docs_dict = {doc["id"]: doc["text"] for doc in docs}
     processed_docs_dict = process_docs(docs_dict)
-    term = get_term(string)
+    terms = get_terms(string.split())
 
     results = []
     for key, value in processed_docs_dict.items():
-        if term not in value:
+        if not any(term in value for term in terms):
             continue
         results.append(key)
 
     sorted_results = sorted(results, key=count_term, reverse=True)
+
     return sorted_results
--- tests/test_search_engine.py
+++ tests/test_search_engine.py
@@ -12,7 +12,7 @@ def test_search_engine():
         {'id': 'doc3', 'text': doc3},
     ]
 
-    assert search(docs, 'shoot') == ['doc1', 'doc2']
+    assert search(docs, 'shoot') == ['doc2', 'doc1']
 
 
 def test_punctuation():
@@ -36,4 +36,19 @@ def test_ranging():
     ]
 
     assert search(docs, 'shoot') == ['doc2', 'doc1']
+
+
+def test_fuzzy_search():
+    doc1 = "I can't shoot straight unless I've had a pint!"
+    doc2 = "Don't shoot shoot shoot that thing at me."
+    doc3 = "I'm your shooter."
+
+    docs = [
+        {'id': 'doc1', 'text': doc1},
+        {'id': 'doc2', 'text': doc2},
+        {'id': 'doc3', 'text': doc3},
+    ]
+
+    assert search(docs, 'shoot at me') == ['doc2', 'doc1']
+
 
